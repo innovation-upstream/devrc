e74f91525d3ff1d3637cea6628fa9c8d8755f686
Zach Lowden <zach@mimobl.com>
2021-12-10 22:55:04 +0000

    add leader t terminal keybind

--- .config/nvim/lua/map/native.lua
+++ .config/nvim/lua/map/native.lua
@@ -4,6 +4,6 @@ map('n', '<leader>k', '<cmd>wincmd k<CR>')
 map('n', '<leader>l', '<cmd>wincmd l<CR>')
 map('n', '<leader>pv', ':wincmd v<bar> :Explore<bar> :vertical resize 30<CR>')
 map('n', '<leader>ee', 'oif err != nil {<CR>return errors.WithStack(err)<CR>}<CR><esc>kk>>_')
-map('n', '<leader>.', ':Files <C-r>=expand("%:h")<CR>/<CR>')
 map('t', '<Esc>', '<C-\\><C-n>')
+map('n', '<leader>t', ':wincmd s<bar> :wincmd j<bar> :resize 10<bar> :terminal<CR>')
 
--- nix/pkgs/tools/k8s/default.nix
+++ nix/pkgs/tools/k8s/default.nix
@@ -4,7 +4,7 @@ with pkgs;
 let
   tilt = buildGoModule rec {
     pname = "tilt";
-    version = "0.23.0";
+    version = "0.23.2";
 
     src = builtins.fetchGit {
       url = "https://github.com/tilt-dev/tilt.git";
