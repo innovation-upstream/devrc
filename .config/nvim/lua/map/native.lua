5986f8dab18a5029b5af711a216c8b9be6d235b8
Zach Lowden <zach@mimobl.com>
2021-12-15 06:51:02 +0000

    upgrade tilt, add git blame binding

--- .config/nvim/lua/map/native.lua
+++ .config/nvim/lua/map/native.lua
@@ -6,4 +6,5 @@ map('n', '<leader>pv', ':wincmd v<bar> :Explore<bar> :vertical resize 30<CR>')
 map('n', '<leader>ee', 'oif err != nil {<CR>return errors.WithStack(err)<CR>}<CR><esc>kk>>_')
 map('t', '<Esc>', '<C-\\><C-n>')
 map('n', '<leader>t', ':wincmd s<bar> :wincmd j<bar> :resize 10<bar> :terminal<CR>')
+map('n', '<leader>b', ':G blame %<CR>')
 
--- nix/pkgs/tools/k8s/default.nix
+++ nix/pkgs/tools/k8s/default.nix
@@ -4,7 +4,7 @@ with pkgs;
 let
   tilt = buildGoModule rec {
     pname = "tilt";
-    version = "0.23.2";
+    version = "0.23.3";
 
     src = builtins.fetchGit {
       url = "https://github.com/tilt-dev/tilt.git";
