c63d695e66dca608dfa5b0fdf1f380674ee531f8
zach <zach@mimobl.com>
2022-12-18 17:46:49 +0100

    lua lsp

--- .config/nvim/lua/nvim_lsp.lua
+++ .config/nvim/lua/nvim_lsp.lua
@@ -115,3 +115,27 @@ nvim_lsp.tailwindcss.setup{
   on_attach = on_attach,
 }
 
+nvim_lsp.sumneko_lua.setup{
+  on_attach = on_attach,
+  settings = {
+   Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
--- nix/pkgs/lang/lua.nix
+++ nix/pkgs/lang/lua.nix
@@ -4,5 +4,6 @@ with pkgs;
 [
   # Lua 5.1 because Neovim uses LuaJIT which is lua 5.1
   lua5_1
+  sumneko-lua-language-server
 ]
 
