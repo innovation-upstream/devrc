cc1b5bde6a26d838c1193a7a6b7a6ea9f90b5277
zach <zach@mimobl.com>
2023-08-18 13:14:09 -0500

    update zsh prompt

--- nix/programs/zsh/default.nix
+++ nix/programs/zsh/default.nix
@@ -11,12 +11,11 @@
   in
     ''
     ${content}
-    PROMPT="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
-    PROMPT+=' %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)'
+    PROMPT='%{$fg_bold[magenta]%}%c%{$reset_color%} $(git_prompt_info)'
 
     ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[green]%}"
     ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
-    ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%} %{$fg[yellow]%}✗"
+    ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%} %{$fg_bold[yellow]%}!"
     ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%}"
     '';
   oh-my-zsh = {
