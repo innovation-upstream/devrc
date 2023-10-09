4d8527215a4b397820fa5cc4eda0234f3bd5597a
Zach Lowden <hello@zacx.dev>
2023-10-09 11:41:10 -0500

    update home manager home.nix init path

--- nix/bin/init-home-manager.sh
+++ nix/bin/init-home-manager.sh
@@ -5,7 +5,7 @@ nix_home_path="${DEVRC_DIR}/nix/home.nix"
 
 NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH TMPDIR=/tmp nix-shell --show-trace '<home-manager>' -A install
 
-cat << EOF > ~/.config/nixpkgs/home.nix
+cat << EOF > ~/.config/home-manager/home.nix
 let
   home-nix-path = /. + builtins.toPath "$(echo "${nix_home_path}")";
   imports = [ home-nix-path ];
