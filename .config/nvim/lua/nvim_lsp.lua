4a45c7b03e47bcad70b414d9b9488684c833b04e
Zach Lowden <hello@zacx.dev>
2025-10-28 18:56:40 -0500

    migrate to new nvim lsp api

--- .config/nvim/lua/nvim_lsp.lua
+++ .config/nvim/lua/nvim_lsp.lua
@@ -1,92 +1,105 @@
-local nvim_lsp = require('lspconfig')
+-- LSP attach autocmd for keymaps and capabilities
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local bufnr = args.buf
 
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+    -- Set omnifunc
+    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+    -- Mappings
+    local opts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rr', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+    vim.keymap.set('n', 'gp', function()
+      vim.diagnostic.goto_prev({ float = true })
+    end, opts)
+    vim.keymap.set('n', 'gn', function()
+      vim.diagnostic.goto_next({ float = true })
+    end, opts)
+    vim.keymap.set('n', 'gl', vim.diagnostic.setloclist, opts)
+    vim.keymap.set('n', 'do', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'dc', 'i' .. client.name, opts)
 
-  -- Mappings.
-  local opts = { noremap=true, silent=true }
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  buf_set_keymap('n', 'gp', '<cmd>lua vim.diagnostic.goto_prev({float=true})<CR>', opts)
-  buf_set_keymap('n', 'gn', '<cmd>lua vim.diagnostic.goto_next({float=true})<CR>', opts)
-  buf_set_keymap('n', 'gl', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-  buf_set_keymap('n', 'do', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', 'dc', 'i' .. client.name, opts)
+    -- Set formatting keymap conditional on server capabilities
+    if client.supports_method('textDocument/formatting') then
+      vim.keymap.set('n', '<space>f', function()
+        vim.lsp.buf.format({ async = true })
+      end, opts)
+    end
 
-  -- Set some keybinds conditional on server capabilities
-  if client.server_capabilities.documentFormattingProvider then
-    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.format { async = true }<CR>", opts)
-  elseif client.server_capabilities.documentRangeFormattingProvider then
-    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.format { async = true }<CR>", opts)
-  end
+    -- Set autocommands conditional on server capabilities
+    if client.supports_method('textDocument/documentHighlight') then
+      vim.api.nvim_set_hl(0, 'LspReferenceRead', { cterm = { bold = true }, ctermbg = 'blue', bg = 'LightYellow' })
+      vim.api.nvim_set_hl(0, 'LspReferenceText', { cterm = { bold = true }, ctermbg = 'blue', bg = 'LightYellow' })
+      vim.api.nvim_set_hl(0, 'LspReferenceWrite', { cterm = { bold = true }, ctermbg = 'blue', bg = 'LightYellow' })
 
-  -- Set autocommands conditional on server_capabilities
-  if client.server_capabilities.documentHighlight then
-    vim.api.nvim_exec([[
-      hi LspReferenceRead cterm=bold ctermbg=blue guibg=LightYellow
-      hi LspReferenceText cterm=bold ctermbg=blue guibg=LightYellow
-      hi LspReferenceWrite cterm=bold ctermbg=blue guibg=LightYellow
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]], false)
-  end
-end
+      local group = vim.api.nvim_create_augroup('lsp_document_highlight', { clear = false })
+      vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
+      vim.api.nvim_create_autocmd('CursorHold', {
+        group = group,
+        buffer = bufnr,
+        callback = vim.lsp.buf.document_highlight,
+      })
+      vim.api.nvim_create_autocmd('CursorMoved', {
+        group = group,
+        buffer = bufnr,
+        callback = vim.lsp.buf.clear_references,
+      })
+    end
 
-nvim_lsp.ts_ls.setup {
-  on_attach = on_attach,
-}
+    -- ESLint auto-fix on save
+    if client.name == 'eslint' then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = bufnr,
+        command = "EslintFixAll",
+      })
+    end
 
-nvim_lsp.eslint.setup {
-  on_attach = function(client, bufnr)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = bufnr,
-      command = "EslintFixAll",
-    })
-    on_attach(client, bufnr)
-  end
-}
-
-nvim_lsp.gopls.setup {
-  on_attach = function(client, bufnr)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = bufnr,
-      command = "lua vim.lsp.buf.format{}",
-    })
-    on_attach(client, bufnr)
+    -- Go format on save
+    if client.name == 'gopls' then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({})
+        end,
+      })
+    end
   end,
-  root_dir = nvim_lsp.util.root_pattern('go.mod')
-}
+})
 
--- The Graphql LSP we get from nixpkgs doesn't seem to work
---nvim_lsp.graphql.setup{
-  --on_attach = on_attach,
---}
+-- TypeScript/JavaScript
+vim.lsp.config('ts_ls', {
+  root_markers = { 'package.json', 'tsconfig.json', 'jsconfig.json', '.git' },
+})
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+-- ESLint
+vim.lsp.config('eslint', {
+  root_markers = { '.eslintrc', '.eslintrc.js', '.eslintrc.json', 'package.json', '.git' },
+})
 
-nvim_lsp.cssls.setup {
-  capabilities = capabilities,
-  cmd = { "css-languageserver", "--stdio" },
-  filetypes = { "css", "scss", "less" },
-  root_dir = nvim_lsp.util.root_pattern("package.json"),
-  on_attach = on_attach,
+-- Go
+vim.lsp.config('gopls', {
+  root_markers = { 'go.mod', '.git' },
+})
+
+-- CSS
+vim.lsp.config('cssls', {
+  cmd = { 'css-languageserver', '--stdio' },
+  filetypes = { 'css', 'scss', 'less' },
+  root_markers = { 'package.json', '.git' },
   settings = {
     css = {
       validate = true
@@ -98,76 +111,78 @@ nvim_lsp.cssls.setup {
       validate = true
     }
   },
-}
+})
 
-nvim_lsp.rust_analyzer.setup{
-  on_attach = on_attach,
-}
+-- Rust
+vim.lsp.config('rust_analyzer', {
+  root_markers = { 'Cargo.toml', '.git' },
+})
 
-nvim_lsp.bashls.setup{
-  on_attach = on_attach,
-}
+-- Bash
+vim.lsp.config('bashls', {
+  root_markers = { '.git' },
+})
 
-nvim_lsp.perlls.setup{
-  on_attach = on_attach,
-}
+-- Perl
+vim.lsp.config('perlls', {
+  root_markers = { '.git' },
+})
 
---nvim_lsp.dhall_lsp_server.setup{
-  --on_attach = on_attach,
---}
+-- Solidity
+vim.lsp.config('solidity_ls', {
+  root_markers = { '.git' },
+})
 
-nvim_lsp.solidity_ls.setup{
-  on_attach = on_attach,
-}
+-- Tailwind CSS
+vim.lsp.config('tailwindcss', {
+  root_markers = { 'tailwind.config.js', 'tailwind.config.ts', 'package.json', '.git' },
+})
 
-nvim_lsp.tailwindcss.setup{
-  on_attach = on_attach,
-}
+-- Cucumber
+vim.lsp.config('cucumber_language_server', {
+  root_markers = { '.git' },
+})
 
-nvim_lsp.volar.setup{
-  on_attach = on_attach,
-  --filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
-  filetypes = {'vue'}
-}
+-- Python
+vim.lsp.config('pyright', {
+  root_markers = { 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', 'Pipfile', '.git' },
+})
 
-nvim_lsp.cucumber_language_server.setup{
-  on_attach = on_attach,
-}
+-- Starlark
+vim.lsp.config('starpls', {
+  root_markers = { '.git' },
+})
 
-nvim_lsp.pyright.setup{
-  on_attach = on_attach,
-}
-
-nvim_lsp.starpls.setup{
-  on_attach = on_attach,
-}
-
--- This is set in sessionVariables.nix
+-- Elixir
 local elixirlspPath = os.getenv("ELIXIR_LSP_PATH")
-nvim_lsp.elixirls.setup{
-  on_attach = on_attach,
-  cmd = { elixirlspPath },
-}
+if elixirlspPath then
+  vim.lsp.config('elixirls', {
+    cmd = { elixirlspPath },
+    root_markers = { 'mix.exs', '.git' },
+  })
+end
 
-nvim_lsp.html.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "html", "templ", "typescriptreact", "javascriptreact" },
-  init_options =  {
+-- HTML
+vim.lsp.config('html', {
+  filetypes = { 'html', 'templ', 'typescriptreact', 'javascriptreact' },
+  root_markers = { 'package.json', '.git' },
+  init_options = {
     embeddedLanguages = {
       css = true,
       javascript = true
     },
-    provideFormatter = true -- this was false before, idk why
+    provideFormatter = true
   },
-}
+})
 
-nvim_lsp.nixd.setup {
-  on_attach = on_attach,
-}
+-- Nix
+vim.lsp.config('nixd', {
+  root_markers = { 'flake.nix', 'default.nix', '.git' },
+})
 
-nvim_lsp.yamlls.setup {
-  on_attach = on_attach,
+-- YAML
+vim.lsp.config('yamlls', {
+  root_markers = { '.git' },
   settings = {
     yaml = {
       schemas = {
@@ -179,31 +194,33 @@ nvim_lsp.yamlls.setup {
       hover = true
     }
   }
+})
+
+-- Enable all configured LSP servers
+local servers = {
+  'ts_ls',
+  'eslint',
+  'gopls',
+  'cssls',
+  'rust_analyzer',
+  'bashls',
+  'perlls',
+  'solidity_ls',
+  'tailwindcss',
+  'cucumber_language_server',
+  'pyright',
+  'starpls',
+  'html',
+  'nixd',
+  'yamlls',
+  'csharp_ls',
 }
 
---[[
-nvim_lsp.sumneko_lua.setup{
-  on_attach = on_attach,
-  settings = {
-   Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-}
-]]--
+if elixirlspPath then
+  table.insert(servers, 'elixirls')
+end
+
+for _, server in ipairs(servers) do
+  vim.lsp.enable(server)
+end
 
