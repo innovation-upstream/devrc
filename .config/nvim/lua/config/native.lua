bc663fd1fd424e761b391ec1a1a2ce33712cf39b
Zach Lowden <zachlowden1@gmail.com>
2026-01-06 19:20:27 -0600

    abs command

--- .config/nvim/lua/config/native.lua
+++ .config/nvim/lua/config/native.lua
@@ -2,3 +2,8 @@ vim.cmd([[
   autocmd BufReadPost * wincmd =
 ]])
 
+-- Custom command to print absolute path of current file
+vim.api.nvim_create_user_command('Abs', function()
+  local abs_path = vim.fn.expand('%:p')
+  print(abs_path)
+end, {})
